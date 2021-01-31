3aa22cf1671bac7cd5cdc3ba61f4bd63f2e4da5a
redat00 <contact@renaudduret.fr>
2021-01-31 22:57:22 +0100

    Enhanced 01_led_blink.py by adding another possible loop to make LED blink

--- 01_led_blink.py
+++ 01_led_blink.py
@@ -25,3 +25,16 @@ while True:
     time.sleep(0.2)
 
 # You should now see your LED blinking !
+
+# There is an other way to make your LED blink
+# it's by using 'not' instead of defining every
+# time the needed value of your LED, when in this
+# case you just want the value which she's not
+
+# For example if your LED is on value 'false'
+# (turned off), what you want is the other value
+# 'true' (turned on). Code implementation :  
+
+# While True:
+#     led.value = not led.value
+#     time.sleep(0.2)
